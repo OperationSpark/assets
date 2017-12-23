c6cb2ca750801d0fae265e4aaa0d738b8cf40b79
Shams Ali <shams.ali0216@gmail.com>
2017-12-23 16:19:25 -0600

    add geoip

--- immersionProjectTests/geoip.js
+++ immersionProjectTests/geoip.js
@@ -2,8 +2,8 @@ var head = document.getElementsByTagName('head')[0];
 var theScript = document.createElement('script');
 theScript.type = 'text/javascript';
 theScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.17.1/axios.js';
-theScript.onreadystatechange = callback;
-theScript.onload = callback;
+// theScript.onreadystatechange = callback;
+// theScript.onload = callback;
 head.appendChild(theScript);
 
 axios.get('http://freegeoip.net/json/')
